c5086596df2142fc703fca0227616ebb7da8269b
Semmmetje <vanderlandsem8@gmail.com>
2026-08-29 02:32:16 +0200

    feat: persist pwarp creation date

--- src/main/java/nl/bandit/warps/model/Warp.java
+++ src/main/java/nl/bandit/warps/model/Warp.java
@@ -1,6 +1,6 @@
 package nl.bandit.warps.model;
-import java.util.*; import org.bukkit.*;
+import java.util.*; import org.bukkit.Location;
 public final class Warp {
- public final String id; public final UUID owner; public String name,description="",icon="default"; public boolean isPublic=true; public final Location location; public final long created=System.currentTimeMillis(); public int visitors; public final Map<UUID,Long> cooldowns=new HashMap<>();
- public Warp(String id,UUID owner,String name,Location location){this.id=id;this.owner=owner;this.name=name;this.location=location;}
+ public final String id; public final UUID owner; public String name,description="",icon="default"; public boolean isPublic=true; public final Location location; public long created; public int visitors; public final Map<UUID,Long> cooldowns=new HashMap<>();
+ public Warp(String id,UUID owner,String name,Location location){this.id=id;this.owner=owner;this.name=name;this.location=location;this.created=System.currentTimeMillis();}
 }
